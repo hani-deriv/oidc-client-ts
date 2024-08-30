714673c3676d63493bb83dd4fcc1af9618cf01bd
hani-deriv <hani@deriv.com>
2024-08-30 10:26:57 +0000

    public client and front channel poc

--- samples/Parcel/src/oidc-client/sample-settings.js
+++ samples/Parcel/src/oidc-client/sample-settings.js
@@ -6,7 +6,7 @@ Log.setLevel(Log.INFO);
 const url = window.location.origin + "/oidc-client";
 
 export const settings = {
-    authority: "http://localhost:1234/oidc",
+    authority: "http://localhost:4567/oidc",
     client_id: "js.tokenmanager",
     redirect_uri: url + "/sample.html",
     post_logout_redirect_uri: url + "/sample.html",
